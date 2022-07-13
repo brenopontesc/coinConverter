deed0a1f6d7195d91725b2154b84b5dd96f467b4
Breno Costa <brenocosta@MacBook-Pro-de-Admin.local>
2022-07-13 10:09:51 -0300

    remove useless dependencies

--- src/product/services/converter/converter.service.ts
+++ src/product/services/converter/converter.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from 'src/product/dtos/Product';
 import { Trade } from 'src/product/dtos/Trade';
-import { AwesomeApiService } from '../awesome-api/awesome-api.service';
 
 @Injectable()
 export class ConverterService {
-    constructor(private awesomeApiService: AwesomeApiService){}
-
     convertProduct(product: Product, trade: Trade): Product{
         let tradeValue : string = Object.values(trade)[0].high
         product.variants.forEach(variant =>{
